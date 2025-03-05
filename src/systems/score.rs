a81a08db732d359aad3ad36372b654a124f82711
Antoine Raoul <tonyraoul6699@gmail.com>
2025-03-05 23:08:33 +1100

    feat: Optimize score display logic by filtering and sorting scores more efficiently

--- src/systems/score.rs
+++ src/systems/score.rs
@@ -5,24 +5,26 @@ pub fn update_score_text(
     mut text_query: Query<&mut Text, With<ScoreText>>,
     score_query: Query<(&BearScore, &Transform)>,
 ) {
-    for mut text in text_query.iter_mut() {
-        let mut scores = score_query
+    if let Some(mut text) = text_query.iter_mut().next() {
+        let mut scores: Vec<(&String, i32)> = score_query
             .iter()
-            .map(|(score, transform)| (score.name.clone(), score.value, transform.translation.y))
-            .collect::<Vec<_>>();
-        
-        // Sort by score value (descending) and filter out fallen bears
-        scores.sort_by(|a, b| b.1.cmp(&a.1));
-        scores.retain(|(_name, _score, y)| *y > -5.0);
+            .filter_map(|(score, transform)| {
+                if transform.translation.y > -5.0 {
+                    Some((&score.name, score.value))
+                } else {
+                    None
+                }
+            })
+            .collect();
 
-        // Update text
-        text.sections[0].value = format!(
-            "Scores:\n{}",
-            scores
-                .iter()
-                .map(|(name, score, _)| format!("{}: {}", name, score))
-                .collect::<Vec<_>>()
-                .join("\n")
-        );
+        scores.sort_by(|a, b| b.1.cmp(&a.1));
+
+        let score_text = scores
+            .iter()
+            .map(|(name, score)| format!("{}: {}", name, score))
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        text.sections[0].value = format!("Scores:\n{}", score_text);
     }
 }
